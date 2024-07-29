f28ad45c2d2c6e7732411fb0e8609e7232f7f8e5
Zedan <zedanmohamedcp@gmail.com>
2024-07-29 17:55:21 +0300

    first 4

--- 584. Find Customer Referee.sql
+++ 584. Find Customer Referee.sql
@@ -2,3 +2,11 @@
 SELECT Customer.name
 FROM Customer 
 WHERE referee_id != 2 OR referee_id IS NULL;
+
+# sol2 not supported in mysql
+
+# Write your MySQL query statement below
+SELECT Customer.name
+FROM Customer 
+WHERE referee_id IS DISTINCT FROM 2;
+
